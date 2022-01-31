d89bea3a2c3c8d21aff578178290a168fc474bb2
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-01-31 21:46:55 +0530

    Fibonacci series

---
+++ LogicalPrograme/FibonacSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograme
+{
+    public class FibonacSeries
+    {
+        public int number;
+        public void Fibb()
+        {
+           int n1 = 0, n2 = 1, n3, i;
+           Console.Write(n1 + " " + n2 + " ");
+           for (i = 2; i < number; ++i)
+           {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+           }
+        }
+    }
+}
---
+++ LogicalPrograme/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LogicalPrograme;
+Console.WriteLine("Hello, Welcome to Logical Programe...! ");
+Console.WriteLine("1. Fibonaccci Series \n2. Exit");
+int option = Convert.ToInt32(Console.ReadLine());
+bool flag = true;
+while(true)
+{
+    
+    switch(option)
+    {
+        case 1:
+            FibonacSeries fibonacSeries = new FibonacSeries();
+            Console.WriteLine("Enter a number upto where you want to print fibbnoci series: ");
+            fibonacSeries.number = Convert.ToInt32(Console.ReadLine());
+            fibonacSeries.Fibb();
+            flag = false;
+            break;
+        case 2:
+            flag = false;
+            break;
+    }
+}
