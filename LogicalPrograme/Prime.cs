0078baf228c9bbb28d2f4e1ce514ff8013d6c905
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-01-31 22:19:30 +0530

    Prime Number

---
+++ LogicalPrograme/Prime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograme
+{
+    public class Prime
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int i;
+        public int m = 0;
+        public int flag = 0;
+        public void Prim()
+        {
+            m = num / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Number is not Prime :- ");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.WriteLine("Number is Prime :- ");
+
+        }
+    }
+}
+
--- LogicalPrograme/Program.cs
+++ LogicalPrograme/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LogicalPrograme;
 Console.WriteLine("Hello, Welcome to Logical Programe...! ");
-Console.WriteLine("1. Fibonaccci Series \n2. Perfect Number \n3. Exit");
+Console.WriteLine("1. Fibonaccci Series \n2. Perfect Number \n3. Prime Number \n4. Exit");
 int option = Convert.ToInt32(Console.ReadLine());
 bool flag = true;
 while(true)
@@ -21,6 +21,11 @@ while(true)
             perfect.Series();
             break;
         case 3:
+            Prime prime = new Prime();
+            Console.WriteLine("Enter a number to check Prime Number or Not...");
+            prime.Prim();
+            break;
+        case 4:
             flag = false;
             break;
     }
